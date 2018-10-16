e3f756e7624c5beb3616842649eb93dd13ac25fb
Matthew Harrilal <matthewharrilal@gmail.com>
2018-10-16 15:32:57 -0700

    Getting response back from manual request

--- controllers/messageWebhook.js
+++ controllers/messageWebhook.js
@@ -3,8 +3,8 @@ const processMessage = require("../helpers/processMessage");
 module.exports = (req, res) => {
     if (req.body.object === "page") {
         req.body.entry.forEach(entry => {
-            console.log('This is the entry ' + JSON.stringify(entry))
             entry.messaging.forEach(event => {
+                console.log('This is the entry ' + JSON.stringify(event))
                 if (event.message && event.message.text) {
                     processMessage(event);
                 }
--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const apiAiClient = require("apiai");
+const express = require("express");
+const app = express()
 const DIALOGFLOW_CLIENT_EMAIL = process.env.DIALOGFLOW_CLIENT_EMAIL
 const FACEBOOK_ACCESS_TOKEN = process.env.FACEBOOK_ACCESS_TOKEN;
 const projectId = 'testagent-bd2d4';
@@ -7,6 +9,13 @@ const sessionId = 'testBot';
 const languageCode = 'en-US';
 const request = require("request");
 const dialogflow = require('dialogflow');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
 const config = {
     credentials: {
         private_key: process.env.DIALOGFLOW_PRIVATE_KEY,
@@ -18,29 +27,45 @@ const sessionClient = new dialogflow.SessionsClient(config);
 
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-const sendTextMessage = (userId, text) => {
-    return fetch(
-        `https://graph.facebook.com/v2.6/me/messages?access_token=${FACEBOOK_ACCESS_TOKEN}`, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                messaging_type: 'RESPONSE',
-                recipient: {
-                    id: userId,
-                },
-                message: {
-                    text,
-                },
-            }),
+function body(userId, text) {
+    const postBody = {
+        "messaging_type" : 'RESPONSE',
+        "recipient": {
+            "id": userId
+        },
+        "message": {
+            text
         }
-    );
+    }
+
 }
+
+const url = `https://graph.facebook.com/v2.6/me/messages?access_token=${FACEBOOK_ACCESS_TOKEN}`
+
+// const sendTextMessage = (userId, text) => {
+//     return fetch(
+//         `https://graph.facebook.com/v2.6/me/messages?access_token=${FACEBOOK_ACCESS_TOKEN}`, {
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             method: 'POST',
+//             body: body
+//         }
+//     );
+// }
+
+const sendTextMessage = (userId, text) => {
+    app.post(url, (req, res) => {
+        req.headers['Content-Type'] = 'application/json'
+        res.send(body(userId, text))
+    })
+};
 
 module.exports = (event) => {
     const userId = event.sender.id;
     const message = event.message.text;
+    console.log("USER ID >>>>>>>>> " + userId)
+    console.log("MESSAGEEE >>>>>>> " + message)
 
     const request = {
         session: sessionPath,
@@ -56,6 +81,7 @@ module.exports = (event) => {
         .detectIntent(request)
         .then(responses => {
             const result = responses[0].queryResult;
+            console.log("RESULT >>>>>> " + result.fulfillmentText)
             return sendTextMessage(userId, result.fulfillmentText);
         })
         .catch(err => {
