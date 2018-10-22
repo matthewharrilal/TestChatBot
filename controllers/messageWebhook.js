2cb1693924756f3593ef73c130ae7eae294d2bf9
Matthew Harrilal <matthewharrilal@gmail.com>
2018-10-22 12:35:50 -0700

    Able to see the message and the result

--- controllers/messageWebhook.js
+++ controllers/messageWebhook.js
@@ -1,31 +1,14 @@
 const processMessage = require("../helpers/processMessage");
 const JSON = require('circular-json')
 
-// function findById(o, id) {
-//     //Early return
-//     if (o.id === id) {
-//         return o;
-//     }
-//     var result, p;
-//     for (p in o) {
-//         if (o.hasOwnProperty(p) && typeof o[p] === 'object') {
-//             result = findById(o[p], id);
-//             if (result) {
-//                 return result;
-//             }
-//         }
-//     }
-//     return result;
-// }
-
 module.exports = (req, res) => {
     req.body.queryResult.fulfillmentMessages.forEach(textObject => {
         console.log('')
-        console.log('MESSAGE >> ' + JSON.stringify(req.body.queryResult.fulfillmentText))
-        console.log('RESULT >> ' + JSON.stringify(textObject.text.text[0]))
+        console.log('MESSAGE >> ' + JSON.stringify(req.body.queryResult.queryText))
         if (req.body.queryResult.outputContexts[0].parameters['CS-Topics']) {
             console.log(req.body.queryResult.outputContexts[0].parameters['CS-Topics'])
         }
+        console.log('RESULT >> ' + JSON.stringify(textObject.text.text[0]))
         console.log('')
         res.status(200).end();
     });
