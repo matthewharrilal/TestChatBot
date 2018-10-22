1e88245841aec4e0c2925fed63265a3b9e0bfbb6
Matthew Harrilal <matthewharrilal@gmail.com>
2018-10-22 12:44:49 -0700

    Extracting the topic of the interview

--- controllers/messageWebhook.js
+++ controllers/messageWebhook.js
@@ -2,14 +2,18 @@ const processMessage = require("../helpers/processMessage");
 const JSON = require('circular-json')
 
 module.exports = (req, res) => {
+    var csTopic;
     req.body.queryResult.fulfillmentMessages.forEach(textObject => {
         console.log('')
+
         console.log('MESSAGE >> ' + JSON.stringify(req.body.queryResult.queryText))
-        if (req.body.queryResult.outputContexts[0].parameters['CS-Topics']) {
-            console.log(req.body.queryResult.outputContexts[0].parameters['CS-Topics'])
-        }
         console.log('RESULT >> ' + JSON.stringify(textObject.text.text[0]))
-        console.log('')
+
+        if (req.body.queryResult.outputContexts[0].parameters['CS-Topics']) {
+            csTopic = req.body.queryResult.outputContexts[0].parameters['CS-Topics']
+        }
+
+        console.log(csTopic)
         res.status(200).end();
     });
 }
